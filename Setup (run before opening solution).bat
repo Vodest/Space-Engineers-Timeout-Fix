32fc39deb868e739bc8a12eb224b9847013b87ee
StalkR <stalkr@stalkr.net>
2024-04-21 11:24:54 +0200

    upgrade csproj to new .NET SDK syntax, remove sln

--- Setup (run before opening solution).bat
+++ Setup (run before opening solution).bat
@@ -1,11 +1,12 @@
-:: This script creates a symlink to the game binaries to account for different installation directories on different systems.
+:: Create junctions (symlinks) to binary references to account for different installation directories on different systems.
+:: Use the dot (.) prefix so dotnet ignores the folder during build.
 
 @echo off
 set /p path="Please enter the folder location of your SpaceEngineers.exe or SpaceEngineersDedicated.exe: "
 
 cd %~dp0
-rmdir GameBinaries > nul 2>&1
-mklink /J GameBinaries "%path%"
+rmdir .GameBinaries > nul 2>&1
+mklink /J .GameBinaries "%path%"
 if errorlevel 1 goto Error
 echo Done!
 
